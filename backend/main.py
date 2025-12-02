6b49b87551de6ca798314d0d321115ef84f713b7
Issac Chawdhury <99365688+IssacMinC@users.noreply.github.com>
2025-12-01 21:38:56 -0600

    added delete

--- backend/main.py
+++ backend/main.py
@@ -93,7 +93,7 @@ def store():
     
     for line in a.getAlpha():
       first_word = line.split()[0]
-      db.insert_kwic_entry(urlID, line, first_word)
+      db.insert_kwic_entry(url, line, first_word)
 
     return {
       "shifts": list(s.getShifts()),
@@ -101,6 +101,17 @@ def store():
       "store": [f"{urlID}: {url}"]
     }
 
+@app.route('/delete', methods=['POST'])
+def delete():
+  data = request.json
+  url = data["url"]
+  try:
+    db.delete_entry(url)
+  except Exception as e:
+    return e
+  return { 'status':'success' }
+
+  
 
 if __name__ == '__main__':
     app.run(debug=True)
